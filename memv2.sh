673a68cf84814bc42aca8630f54d7130dac7c35e
jdafcorp2008 <jdafcorp2008@gmail.com>
2018-11-16 14:44:19 -0500

    segunda mejora aws put-metrics para puerto 20000

--- memv2.sh
+++ memv2.sh
@@ -3,7 +3,7 @@ USEDMEMORY=$(free -m | awk 'NR==2{printf "%.2f\t", $3*100/$2 }')
 TCP_CONN=$(netstat -an | wc -l)
 TCP_CONN_PORT_80=$(netstat -an | grep 80 | wc -l)
 TCP_CONN_PORT_20000=$(netstat -an | grep 20000 | wc -l)
-INSTANCE_ID  = $(echo '$INSTANCEID')
+INSTANCE_ID=$(echo '$INSTANCEID')
  
 aws cloudwatch put-metric-data --metric-name memory-usage --dimensions Instance=$INSTANCE_ID  --namespace Maestria --value $USEDMEMORY
 aws cloudwatch put-metric-data --metric-name Tcp_connections --dimensions Instance=$INSTANCE_ID  --namespace Maestria --value $TCP_CONN
